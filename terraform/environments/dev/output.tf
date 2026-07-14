f0b22ff1e2025279632c5178647babc282886914
phong.tran <phong.tran-ost@asiapac.com.sg>
2026-07-14 10:41:08 +0700

    update: show info tf

---
+++ terraform/environments/dev/output.tf
@@ -0,0 +1,16 @@
+
+output "vpc_id" {
+  value       = module.vpc.vpc_id
+}
+
+output "public_subnet_ids" {
+  value       = module.vpc.public_subnet_ids 
+}
+
+output "app_subnet_ids" {
+  value       = module.vpc.app_subnet_ids 
+}
+
+output "data_subnet_ids" {
+  value       = module.vpc.data_subnet_ids 
+}
